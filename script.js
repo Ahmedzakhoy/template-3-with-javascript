eaca0d8312f4416a00c360cc729214041f5afc09
Ahmed Mustafa <Ahmedzakho925@gmail.com>
2022-06-27 22:11:46 +0300

    add countdown Timer and fix bugs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
+////////////////////////////////////////////////////////////
+//imgs lazy loading
+
 //selections
 const AllSections = document.querySelectorAll(".section");
 const AllImgs = document.querySelectorAll(".section img");
-
-////////////////////////////////////////////////////////////
-//imgs lazy loading
 
 //change img src and blur
 AllImgs.forEach((img) => {
@@ -39,7 +39,10 @@ const sectionObserver = new IntersectionObserver(loadImg, {
 
 //implement observe() method
 AllSections.forEach((section) => sectionObserver.observe(section));
+
 ////////////////////////////////////////////////////////////
+// mega menu on click event
+
 //selection
 const megaMenu = document.querySelector(".mega-menu");
 const megaMenuHyperlink = document.querySelector(".mega-menu-a");
@@ -47,7 +50,6 @@ const megaMenuLink = document.querySelector(
   "header .container nav > ul > li:last-child"
 );
 const overlay = document.querySelector(".overlay");
-// mega menu on click event
 //prevent hyperlink default behaviour
 megaMenuHyperlink.addEventListener("click", function (event) {
   event.preventDefault();
@@ -82,18 +84,19 @@ megaMenuMedia(media); // Call listener function at run time
 media.addEventListener("change", megaMenuMedia); // Attach listener function on state changes
 
 ////////////////////////////////////////////////////////////
+//sticky header implementation
+
 //selections
 const header = document.querySelector("header");
 const headerNav = document.querySelector(".header-nav");
 const landing = document.querySelector(".landing");
-//sticky header implementation
 
 // get height of element
 const headerHeight = header.getBoundingClientRect().height;
 // callback function for observer
 
 //parent function for implementation of sticky nav with different element and class name
-function stickyNavParentFunction(element, className) {
+function stickyNavParentFunction(element, className, match = false) {
   const stickyNav = function (entries) {
     const [entry] = entries;
     if (!entry.isIntersecting) {
@@ -102,8 +105,8 @@ function stickyNavParentFunction(element, className) {
       megaMenu.classList.remove("show-mega-menu-media");
     } else {
       element.classList.remove(className);
-      overlay.style.top = "148px";
-      megaMenu.classList.add("show-mega-menu-media");
+      overlay.style.top = match && entry.isIntersecting ? "148px" : "74px";
+      match ? megaMenu.classList.add("show-mega-menu-media") : "";
     }
   };
   const headerObserver = new IntersectionObserver(stickyNav, {
@@ -118,7 +121,7 @@ function stickyNavParentFunction(element, className) {
 function stickyNavMedia(media) {
   if (media.matches) {
     // If media query matches
-    stickyNavParentFunction(headerNav, "sticky-nav-of-header");
+    stickyNavParentFunction(headerNav, "sticky-nav-of-header", true);
   } else {
     stickyNavParentFunction(header, "sticky-header");
   }
@@ -126,3 +129,36 @@ function stickyNavMedia(media) {
 const mediaNav = window.matchMedia("(max-width: 767px)");
 stickyNavMedia(mediaNav); // Call listener function at run time
 mediaNav.addEventListener("change", stickyNavMedia); // Attach listener function on state changes
+
+////////////////////////////////////////////////////////////
+//countDown timer
+
+//selections
+const daysElement = document.querySelector(".days span");
+const hoursElement = document.querySelector(".hours span");
+const minutesElement = document.querySelector(".minutes span");
+const secondsElement = document.querySelector(".seconds span");
+
+// get future year dynamically from current year
+let year = new Date().getFullYear() + 1;
+// future ISO String time
+const futureDate = `${year}-03-05T18:05:47.557Z`;
+//callback function fot set intveral
+function timer() {
+  const date2 = new Date(futureDate).getTime();
+  const date = Date.now();
+  const durationInS = (date2 - date) / 1000;
+  const timeInDays = Math.trunc(durationInS / (60 * 60 * 24));
+  const timeInHours = Math.trunc((durationInS % (60 * 60 * 24)) / (60 * 60));
+  const timeInMinutes = Math.trunc((durationInS % (60 * 60)) / 60);
+  const timeInSeconds = Math.trunc(durationInS % 60);
+  if (durationInS <= 0) {
+    clearInterval(countDownInterval);
+  }
+  daysElement.textContent = timeInDays;
+  hoursElement.textContent = timeInHours;
+  minutesElement.textContent = timeInMinutes;
+  secondsElement.textContent = timeInSeconds;
+}
+//set interval timer
+const countDownInterval = setInterval(timer, 1000);
